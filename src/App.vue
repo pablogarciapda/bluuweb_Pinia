6d2a010a5a208f6d68bd0ffb4b525d9fa1f19877
Pablo Garcia <31891934+pablogarciapda@users.noreply.github.com>
2022-03-29 20:12:42 +0200

    inicio

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>App base</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login"> Login</router-link> |
+      <router-link to="/register"> Register</router-link> |
+      <button>Logout</button>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup></script>
